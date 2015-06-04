2f1c51b7e4c3c58b9b2e6589192c549e241a8a1d
fbrito <fbrito@sb-10-16-10-53.dev.terradue.int>
2015-06-04 16:21:05 +0200

    added pixex

--- src/main/app-resources/smacop/run.sh
+++ src/main/app-resources/smacop/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -x
 # source the ciop functions (e.g. ciop-log)
 source ${ciop_job_include}
 
@@ -48,6 +48,15 @@ uH2o="`ciop-getparam uH2o`"
 uO3="`ciop-getparam uO3`"
 useMerisADS="`ciop-getparam useMerisADS`"
 
+#
+# get the POIs
+echo "`ciop-getparam poi | tr "," "\t"`" > $TMPDIR/poi.csv
+
+# get the window size
+window="`ciop-getparam window`"
+# get the aggregation
+aggregation="`ciop-getparam aggregation`"
+
 
 # loop and process all MERIS products
 while read inputfile 
@@ -82,12 +91,47 @@ outputname=`basename $retrieved`
   
   res=$?
   [ $res != 0 ] && exit $ERR_BEAM
+
+  evaluate="true"
+  publish_l2="true"
+ 
+  run=${CIOP_WF_RUN_ID}
+ 
+  [ "${evaluate}" == "true" ] && {
+    # invoke pixex
+    l2b="$( basename $OUTPUTDIR/$outputname )"
+    prddate="${l2b:20:2}/${l2b:18:2}/${l2b:14:4}"
+    ciop-log "INFO" "Apply BEAM PixEx Operator to ${l2b}"
   
-  tar -C $OUTPUTDIR -cvzf $TMPDIR/$outputname.tgz $outputname.dim $outputname.data
+    # apply PixEx BEAM operator
+    $_CIOP_APPLICATION_PATH/shared/bin/gpt.sh \
+  	-Pvariable=$outputname.dim \
+  	-Pvariable_path=$OUTPUTDIR \
+  	-Poutput_path=$OUTPUTDIR \
+        -Pprefix=$run \
+  	-Pcoordinates=$TMPDIR/poi.csv \
+	-PwindowSize=$window \
+	-PaggregatorStrategyType="$aggregation" \
+	${_CIOP_APPLICATION_PATH}/pixex/libexec/PixEx.xml &> /tmp/`uuidgen`.log #dev/null		
+  	
+    res=$?
+    [ $res != 0 ] && exit $ERR_BEAM_PIXEX 
+ 
+    ciop-log "DEBUG" "`tree $OUTPUTDIR`" 
+    ciop-log "DEBUG" "`tree $TMPDIR`"
+    result="`find $OUTPUTDIR -name "$run*measurements.txt"`"
+    cat "$result" |  tail -n +7 | tr "\t" "," | awk -f $_CIOP_APPLICATION_PATH/pixex/libexec/tidy.awk -v run=$run -v date=$prddate - > $TMPDIR/csv
+    mv $TMPDIR/csv "$OUTPUTDIR/$l2b.txt" 
   
-  ciop-log "INFO" "Publishing $outputname.tgz"
-  ciop-publish -m $TMPDIR/$outputname.tgz
-  
+    ciop-log "INFO" "Publishing extracted pixel values"
+    ciop-publish -m "$OUTPUTDIR/$l2b.txt"
+  }
+
+  [ "${publish_l2}" == "true" ] && {
+    tar -C $OUTPUTDIR -cvzf $TMPDIR/$outputname.tgz $outputname.dim $outputname.data
+    ciop-log "INFO" "Publishing $outputname.tgz"
+    ciop-publish -m $TMPDIR/$outputname.tgz
+  }
   # cleanup
   rm -fr $retrieved $OUTPUTDIR/$outputname.d* $TMPDIR/$outputname.tgz 
 
